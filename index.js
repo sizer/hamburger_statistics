a4c66398defecbeac6250708c961de29dcc6a149
sizer <shinse.tanaka@gmail.com>
2019-01-26 15:20:55 +0900

    read ch.1

---
+++ index.js
@@ -0,0 +1,10 @@
+const add = (x, y) => x + y;
+
+const mean = numbers => numbers.reduce(add) / numbers.length;
+
+const sample_variance = numbers => {
+  const m = mean(numbers);
+  return mean(numbers.map(x => (m - x) ** 2));
+}
+
+const standard_deviation = numbers => Math.sqrt(sample_variance(numbers));
